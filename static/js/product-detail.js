46bfbdb1e8295391f12e4d283cc05d93eb9dcdf6
Leman Mirizade <leman.miriyeva02@mail.ru>
2024-08-25 14:12:33 +0400

    initial commit

--- static/js/product-detail.js
+++ static/js/product-detail.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             showInput: false,
             showPalette: true,
             palette: [
-                ["black", "white", "red", "green", "blue"],
+                ["rgb(0,0,0)", "white", "red", "green", "blue"],
                 ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
                 ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)", "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)", "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)", "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
             ]
@@ -46,17 +46,48 @@ $("#preferredHex1").spectrum({
     ]
 });
 
-$( document ).ready(function() {
-    $("#preferredHex").on('change', function(){
-        var colorVal =  $("#preferredHex").val();
-        let element2=document.querySelectorAll('.sofa-section img')
+// $( document ).ready(function() {
+//     $("#preferredHex").on('change', function(){
+//         var colorVal =  $("#preferredHex").val();
+//         let element2=document.querySelectorAll('.sofa-section img')
        
-        for(let i=0;i<element2.length;i++){
-            element2[i].src=`${location.origin}/static/images/sofa-${colorVal}.png`
-        }
-      });
+//         for(let i=0;i<element2.length;i++){
+//             element2[i].src=`${location.origin}/static/images/sofa-${colorVal}.png`
+//         }
+//       });
           
-})
+// })
+
+$(document).ready(function() {
+    // Predefined list of image filenames
+    let imageList = [
+        '000000.png',
+        'ffffff.png',
+        
+    ];
+
+    $("#preferredHex").on('change', function() {
+        var colorVal = $("#preferredHex").val().replace('#', ''); 
+        console.log(colorVal);
+        
+        let elements = document.querySelectorAll('.sofa-section img');
+
+        // Construct the new image filename based on the selected color value
+        let newImage = `${colorVal}.png`;
+        console.log(newImage);
+        
+        // Check if the new image exists in the predefined list
+        if (imageList.includes(newImage)) {
+            elements.forEach(function(element) {
+                element.src = `${location.origin}/static/images/${colorVal}.png`;
+            });
+        } else {
+            console.warn("Image not found in the list for the selected color:", colorVal);
+        }
+    });
+});
+
+
 $( document ).ready(function() {
     $("#preferredHex1").on('change', function(){
         var colorVal =  $("#preferredHex1").val();
